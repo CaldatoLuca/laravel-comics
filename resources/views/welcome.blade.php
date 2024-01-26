42c74802ecc1f6a43c05ec1a32333a37f04e8b79
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-26 19:14:31 +0100

    feat: main welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.main')
 
 @section('main')
-<h1>Main welcome</h1>
+    {{-- jumbotron --}}
+    <section class="jumbotron"></section>
+
+    {{-- cards --}}
+    <section class="current-series">
+        <h2>Current Series</h2>
+        <div class="container">
+            <div class="comics d-flex">
+                @foreach ($comics as $comic)
+                    <div class="element">
+                        <div class="image">
+                            <img src="{{ Vite::asset($comic['thumb']) }}" alt="{{ $comic['title'] }}">
+                        </div>
+                        <div class="text">{{ $comic['title'] }}</div>
+                    </div>
+                @endforeach
+            </div>
+            <button class="btn btn-main text-uppercase">Load More</button>
+        </div>
+    </section>
 @endsection
